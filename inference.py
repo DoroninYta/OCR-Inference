c70803820816bd7e3a33b41ddaae2e7da550b868
DorononYta <doronin.iua@inviewlab.com>
2025-06-08 17:11:06 +0300

    inference.py: created minimal class for detecting

--- inference.py
+++ inference.py
@@ -1,30 +1,33 @@
-# all proccess - load models: detector+ trcker, plate segmentor, ocr
-# later append api using 
-# import cv2 as cv
-#Import only if not previously imported
-#Import only if not previously imported
-import cv2
-# Create a Video Reader Object.
-cap = cv2.VideoCapture("/home/user/3_numbers/ocr_show_work/video/3.mp4")
-if cap.isOpened() == False:
-    print("Error in opening video stream or file")
-#Define the codec for the Video
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#Create Video Writer Object
-writer = cv2.VideoWriter("/home/user/3_numbers/ocr_show_work/video/3.avi",fourcc, 30, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-frame_number = 0
-while cap.isOpened():
-    frame_number += 1
-    ret, frame = cap.read()
-    if frame_number > 
-    if ret:
-        writer.write(frame)
-        cv2.imshow("Frame",frame)
-        # Exit on pressing esc
-        if cv2.waitKey(20) & 0xFF == 27:
-            break
-    else:
-        break
-cap.release()
-writer.release()
-cv2.destroyAllWindows()
+import ultralytics
+import torch
+import cv2 as cv
+from pathlib import Path
+from ultralytics import YOLO
+class NumberPlateOCR:
+    def __init__(self, path_to_video: Path,  ):
+        self.model = YOLO("yolov8m.pt")
+        self.cap = cv.VideoCapture(path_to_video)
+    
+    def  video_runner(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                result = self.predict(frame)
+                gt_imge = result[0].plot()
+                cv.imshow("Frame", gt_imge)
+                # Exit on pressing esc
+                if cv.waitKey(20) & 0xFF == 27:
+                    break
+            else:
+                break
+            
+    def predict(self, image):
+        results = self.model.predict(image, stream=False, save = False)
+        return results
+    
+def main():
+    number_plate_ocr = NumberPlateOCR(Path("/home/user/3_numbers/ocr_show_work/video/53.avi"))
+    number_plate_ocr.video_runner()
+
+if __name__ == "__main__":
+    main()
